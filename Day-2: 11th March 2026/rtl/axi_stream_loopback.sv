b729d299ea4f879c64cf7c444bfa2abe0ad057bf
bit2silicon <saishankarash2602@gmail.com>
2026-03-12 12:25:12 +0530

    Day-2 11th March 2026 - Integrated my axi stream loopback module with Processing System and Direct Memory Access - PS <---> DMA <---> Axi stream loopback module

---
+++ Day-2: 11th March 2026/rtl/axi_stream_loopback.sv
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/09/2026 01:56:55 PM
+// Design Name: 
+// Module Name: ldpc_stream_loopback
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ldpc_stream_loopback(
+    input  logic clk,
+    input  logic rst_n,
+    input  logic [31:0] s_axis_tdata,
+    input  logic        s_axis_tvalid,
+    output logic        s_axis_tready,
+    input  logic        s_axis_tlast,
+    output logic [31:0] m_axis_tdata,
+    output logic        m_axis_tvalid,
+    output logic        m_axis_tlast,
+    input  logic        m_axis_tready
+    );
+    
+    reg stlast;
+    assign s_axis_tready = !m_axis_tvalid || m_axis_tready;
+    assign m_axis_tlast  = m_axis_tvalid & stlast;
+    
+    always_ff@(posedge clk) begin 
+        if(!rst_n) begin
+            stlast <= 0;
+        end
+        else begin
+        if(s_axis_tlast) begin
+            stlast <= 1;
+        end
+        else if (m_axis_tlast && s_axis_tready) begin
+            stlast <= s_axis_tlast;
+        end
+        end
+    end
+    
+    always_ff@(posedge clk) begin
+        if(!rst_n) begin
+            m_axis_tvalid <= 0;
+        end
+        else begin
+        if(s_axis_tready) begin
+            m_axis_tvalid <= s_axis_tvalid;
+            if(s_axis_tvalid)
+            m_axis_tdata  <= s_axis_tdata;
+        end
+//        else if(m_axis_tvalid && m_axis_tready) begin
+//            m_axis_tvalid <= 0;
+//        end
+        end
+    end
+    
+    
+    
+endmodule
